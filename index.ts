949570dd06fc26af14a3d5c6341aa1856803973b
ali-haider-381 <a1aichannel@gmail.com>
2023-08-29 20:51:20 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,43 @@
+"use strict"
+/*Print a message like "Hey Abu Hurairah, I have started learning TYPESCRIPT*/
+console.log("Hey Abu Hurairah, I have started learning TYPESCRIPT");
+
+/*Print a message like "Hey Abu Hurairah, I have started learning TYPESCRIPT."*/
+console.log("I have started learning TYPESCRIPt");
+
+/*Store your name in a variable and print it.*/
+var myname:string="ali haider"
+/*console.log(myname);*/
+
+/*Store 10 numbers in different variables*/
+var username:string="ali haider";
+var a=1;
+var b=1;
+var c=1;
+var d=1;
+var e=1;
+var f=1;
+var g=1;
+var h=1;
+var i=1;
+var j=1;
+var sum=a+b+c+d+e+f+g+h+i+j;
+var sub=a-b-c-d-e-f-g-h-i-j;
+var mul=a*b*c*d*e*f*g*h*i*j;
+var div=g/a;
+/*Add all of them and print the SUM.*/
+console.log("Sum is",sum);
+/*Print the Difference (subtraction)*/
+console.log("Subtraction is",sub);
+/*Print the result after multiplying al*/
+console.log("Multiplication is",mul);
+/*Take two numbers and print the division result.*/
+console.log("Division of g and a is",div);
+/*perform all four operations with the given numbers, print the results, and observe the order in which the operations take place.*/
+console.log(2-6*13+8/5);
+
+
+
+
+
+
